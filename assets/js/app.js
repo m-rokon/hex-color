baa28b8aad6be80bb78ab6f09d1ba7f8cdc0b7a0
Mohammad Al Mamun Rokon <rokon@Rokons-MBP.local>
2022-02-24 03:44:33 +0600

    live push with all working code

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+const body = document.querySelector('body');
+const btn = document.querySelector('.btn');
+const hex_numbers = [0,1,2,3,4,5,6,7,8,9,'A', 'B', 'C', 'D', 'E', 'F'];
+
+btn.addEventListener('click', changeHex);
+
+function changeHex () {
+    let hex = '#';
+    for( let i=0; i<6; i++ ) {
+        let index = Math.floor(Math.random() * hex_numbers.length);
+        hex += hex_numbers[index];
+    }
+    body.style.backgroundColor = hex;
+}
